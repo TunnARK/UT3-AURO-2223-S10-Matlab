acca6a5ec9094fea0f10355ffbba9ea7c600c05f
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-09-21 10:38:20 +0200

    UP: Q2.2 Initial commit

--- VisionIndustrielle/VIRCRV-TP1/VIRCRV_TP1_main.m
+++ VisionIndustrielle/VIRCRV-TP1/VIRCRV_TP1_main.m
@@ -42,7 +42,7 @@ t=[0:0.05:4];
 
 %% CONFIGURATION
 % Choix de la config de test parmi celles d�finies au-dessus (ou d'autres � votre discr�tion)
-q = [pi pi/2 -1 0];
+q =Q3;
 
 %% SECTION 2.1
 % Matrices de passages homogenes
@@ -68,28 +68,40 @@ T34=[
     0           0           0   1];
 
 % Modele MGD
-T04 = T01 * T12 * T23 * T34 
+T04 = T01 * T12 * T23 * T34
 
 % Verfication du MGD
 v_test = T04 * [1   ; 0.5  ; 1   ; 1]
 
-
 %% SECTION 2.2
 
-	% Calculer ici TOC la matrice de passage homog�ne entre R0 et Rc
+% Calcul de la matrice de passage de R2 à Rc
+T2C=[
+    1 0 0 L2+L3;
+    0 1 0  0 ;
+    0 0 1  0;
+    0 0 0 1 ]; 
+    
+T0C= T01*T12*T2C % Matrice de passage homgene entre R0 et RC
 
 
-	% D�termination de la situation de la pi�ce dans R0
-	% Donn�es: position de la pi�ce dans Rc : (xP, yP, h), orientation : cf. �nonc�
-  	% Afficher la position (X,Y,Z) de la pi�ce dans R0 --> Fonction : plot3(X,Y,Z,'ro'); 
-	% D�duire la situation de la pi�ce dans R0
-  	% Utilisation de la fonction drawBM
+% Affichage de l'orientation de Rc
+drawBM(Q1)
+hold on
+drawFrame(T0C,'RC')
+hold off
 
-	% Calcul du MGD et afficher le rep�re outil
-	% Ne pas oublier hold on si n�cessaire pour la superposition des courbes
+% D�termination de la situation de la pi�ce dans R0
+% Donn�es: position de la pi�ce dans Rc : (xP, yP, h), orientation : cf. �nonc�
+% Afficher la position (X,Y,Z) de la pi�ce dans R0 --> Fonction : plot3(X,Y,Z,'ro'); 
+% D�duire la situation de la pi�ce dans R0
+% Utilisation de la fonction drawBM
 
-  	% Calcul du MGI et v�rification
-  	% Affichage
+% Calcul du MGD et afficher le rep�re outil
+% Ne pas oublier hold on si n�cessaire pour la superposition des courbes
+
+% Calcul du MGI et v�rification
+% Affichage
 
 
 %% SECTION 2.3
