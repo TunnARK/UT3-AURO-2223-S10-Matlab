be947793e72c6b07edb11564827951f71a8f3882
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-09-26 12:32:52 +0200

    UP : Commande par retour d'état

--- VisionIndustrielle/VIRCRV-TP1/VIRCRV_TP1_main.m
+++ VisionIndustrielle/VIRCRV-TP1/VIRCRV_TP1_main.m
@@ -102,28 +102,63 @@ Qsol1=Qsol(:,1) ; % sol1: configuration du robot pour atteindre la piece
 Qsol2=Qsol(:,2) ; % sol2: configuration du robot pour atteindre la piece
 
 % Affichage MGI
-figure(1)
-subplot(1,2,1)
-drawBM(Qsol1)
-drawFrame(calculT04(Qsol1),'Routils',0.3)
-subplot(1,2,2)
-drawBM(Qsol2)
-drawFrame(calculT04(Qsol2),'Routils',0.3)
-
-
-% Calcul du MGD et afficher le rep�re outil
-% Ne pas oublier hold on si n�cessaire pour la superposition des courbes
-
-% Calcul du MGI et v�rification
+% figure(1)
+% subplot(1,2,1)
+% drawBM(Qsol1)
+% drawFrame(calculT04(Qsol1),'Routils',0.3)
+% subplot(1,2,2)
+% drawBM(Qsol2)
+% drawFrame(calculT04(Qsol2),'Routils',0.3)
 
 
 
 %% SECTION 2.3
 
 	% D�termination des repr�sentations d'�tat des actionneurs
-
     
+    A1=[0 1 ; 0 -B/Jeff1];
+    B1=[0; Km/R*Jeff1];
+    C1=[1 0];
+    
+    A2=[0 1 ; 0 -B/Jeff2];
+    B2=[0; Km/R*Jeff2];
+    C2=[1 0];
+    
+    D=0;
+    
+    EE1=ss(A1,B1,C1,D);
+    EE2=ss(A2,B2,C2,D);
+    
+    q1_star=Qsol1(1);
+    q2_star=Qsol1(2);
+    
+        
 	% R�ponse indicielle des actionneurs seuls
+       
+%subplot(1,2,1)
+%plot(q1_star*step(EE1))
+%subplot(1,2,2)
+%plot(q2_star*step(EE2))
 
+        
+    %Calcul du retour d etat
+    
+Ar1=A1-B1*K1;
+Br1=B1*N1*q1_star;
+
+Ar2=A2-B2*K2;
+Br2=B2*N2*q2_star;
+
+retour_et1=ss(Ar1,Br1,C1,D);
+retour_et2=ss(Ar2,Br2,C2,D);
+
+% subplot(1,2,1)
+% plot(q1_star*step(retour_et1))
+% subplot(1,2,2)
+% plot(q2_star*step(retour_et2))
 	
 	% R�ponse indicielle de chaque actionneur et dessin de la trajectoire
+    
+
+drawTraj(retour_et1,retour_et2,q1_star,q2_star,[0:20],0.8,P_R0(1),P_R0(2),P_R0(3),1)
+    
