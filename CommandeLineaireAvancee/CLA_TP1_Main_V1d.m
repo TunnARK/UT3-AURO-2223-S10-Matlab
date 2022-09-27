37ed6b7f31194943047025fa312369d66406b06d
TunnARK <96030186+TunnARK@users.noreply.github.com>
2022-09-27 08:23:57 +0200

    UP: added fixed variables

--- CommandeLineaireAvancee/CLA_TP1_Main_V1d.m
+++ CommandeLineaireAvancee/CLA_TP1_Main_V1d.m
@@ -1,3 +1,11 @@
 % ------------------------ %
 % CLA TP1 GABRIEL-RONK V1d %
 % ------------------------ %
+
+% VARIABALES
+S       = 0,0154    % m^2
+Sn      = 0.00005   % m^2
+Hmax    = 0.6       % m
+k       = 160000    % cst
+b       = -9.2592   % cst
+Qmax    = 0.00012   % m^3/s quand tension = +10V
