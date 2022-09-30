5cc415e570c34c955f185f056726f6d128603a63
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-09-30 08:41:44 +0200

    CR TP1 Q4.1.1+2

--- CommandeLineaireAvancee/CLA_TP1_Main_V1d.m
+++ CommandeLineaireAvancee/CLA_TP1_Main_V1d.m
@@ -21,17 +21,47 @@ Qmax    = 12 * 10^(-5)      ; % m^3/s quand tension = +10V
 az13 = 0.4753 ;
 az32 = 0.4833 ;
 az20 = 0.9142 ;
-a13 = az13 * Sn * sqrt( 2*9.81 ) 
-a32 = az32 * Sn * sqrt( 2*9.81 ) 
-a20 = az20 * Sn * sqrt( 2*9.81 ) 
+a13 = az13 * Sn * sqrt( 2*9.81 ) ;
+a32 = az32 * Sn * sqrt( 2*9.81 ) ;
+a20 = az20 * Sn * sqrt( 2*9.81 ) ;
 
-% Resistance
-%R13 = sqrt( abs( H10 - H30 ) ) / a13 ;
-%R32 = sqrt( abs( H30 - H20 ) ) / a32 ;
-%R20 = sqrt( abs( H20 ) ) / a20 ;
 
 %% Point Equilibre
 
-H2eq = ( (Q1eq)^2 + (Q2eq)^2 )/(a20)^2
-H1eq = (Q1eq)^2/(a13)^2 + (Q1eq)^2/(a32)^2 + H2eq
-H3eq = (Q1eq)^2/(a32)^2 + H2eq
+H2eq = ( (Q1eq)^2 + (Q2eq)^2 )/(a20)^2;
+H1eq = (Q1eq)^2/(a13)^2 + (Q1eq)^2/(a32)^2 + H2eq;
+H3eq = (Q1eq)^2/(a32)^2 + H2eq;
+
+% Resistance
+R13 = sqrt( abs( H1eq - H3eq ) ) / a13 ;
+R32 = sqrt( abs( H3eq - H2eq ) ) / a32 ;
+R20 = sqrt( abs( H2eq ) ) / a20 ;
+
+
+
+
+%% Modele EE 
+A=[-1/S*R13 , 1/S*R13 , 0 ; 1/S*R13 , -1/S*(1/R13+1/R32) , 1/S*R32; 0, 1/S*R32, -1/S*(1/R13+1/R20)];
+B=[1/S, 0; 0 , 0 ; 0, 1/S];
+C=[1, 0, 0];
+D=0;
+
+EE=ss(A,B,C,D)
+
+%% Analyse de stabilite
+
+eig(A)
+
+%% Commandabilite 
+
+% En considérant l entree q1
+
+rangCq1=rank(ctrb(A,B(:,1)))
+
+% En considérant l entree q2
+
+rangCq2=rank(ctrb(A,B(:,2)))
+
+% En considerant les deux entree
+rangC=rank(ctrb(A,B))
+
