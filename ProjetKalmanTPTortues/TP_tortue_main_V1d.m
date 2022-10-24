5261a89c4845a862fd578758abd44002264a1ed9
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-10-24 15:17:51 +0200

    TP Projet : Fichier V1 Damien

---
+++ ProjetKalmanTPTortues/TP_tortue_main_V1d.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TP - Migration des tortues luth
+%RONK Antoine
+% Damien GABRIEL CALIXTE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+close all
+clean all
+load turt8.data
+
