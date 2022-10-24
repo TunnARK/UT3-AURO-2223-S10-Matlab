f3bf7da5cc14037b93f3a595f42b80d350a69b5f
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-10-24 16:52:26 +0200

    TP P1K : Début Q3

--- ProjetKalmanTPTortues/TP_tortue_main_V1d.m
+++ ProjetKalmanTPTortues/TP_tortue_main_V1d.m
@@ -7,5 +7,21 @@
 clc
 close all
 clean all
+
+%% Remplissage des donnnees 
 load turt8.data
+[N,T,Z,u,F,G,H,mX0,PX0,Qw,Rv,X]=simulationDonnees;
+
+
+
+%% Filtrage de Kalman 
+
+% Initialisation
+
+x_obsv={mX0}; 
+P_obsv={PX0};
+
+% Prediction
+
+
 
