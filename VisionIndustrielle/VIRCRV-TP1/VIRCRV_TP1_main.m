40436e851fa2881fd01a7d5387784b5661ea4ad8
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-10-24 09:30:59 +0200

    VI TP1 : Version finale du code

--- VisionIndustrielle/VIRCRV-TP1/VIRCRV_TP1_main.m
+++ VisionIndustrielle/VIRCRV-TP1/VIRCRV_TP1_main.m
@@ -53,6 +53,7 @@ T04 = calculT04(q);
 q_test21 = [pi pi/2 -1 0] ;
 T04q = calculT04(q_test21);
 v_test21 = T04q * [1   ; 0.5  ; 1   ; 1] ;
+figure(1)
 drawBM(q_test21) ;
 hold on
 drawFrame(T04q,'R4',0.3) ; % repere scaled at 0.3
@@ -69,6 +70,7 @@ hold off
 T0C= calculT0C(q)
 
 % Affichage de l'orientation de Rc
+figure(2)
 drawBM(Q1) ;
 hold on
 drawFrame(T0C,'RC',0.1) ; % repere scaled at 0.1
@@ -81,7 +83,7 @@ hold off
 P_R0=T0C*[xP; yP ;h; 1]; % position objet dans RO
 P_Rp=[xP; yP ;h; 1]; % position objet donnee par la camera dans Rc
 
-% position objet donnee par la camera dans R0
+% Position objet donnee par la camera dans R0
 Rp=[
     1   0   0   P_R0(1)   ; % pas de rotation sur xyz car meme orientation que R0
     0   1   0   P_R0(2)   ; 
@@ -89,10 +91,11 @@ Rp=[
     0   0   0   1    ]
 
 % Validation
+figure(3)
 drawBM(q)
 hold on
-drawFrame(Rp,'Rcp',0.3)
-drawFrame(T0C,'Rop',0.3)
+drawFrame(Rp,'Rcp',0.1)
+drawFrame(T0C,'Rop',0.1)
 hold off
 
 % Q2.2.3 cf compte rendu
@@ -105,19 +108,15 @@ Qsol1=Qsol(:,1) ; % sol1: configuration du robot pour atteindre la piece
 Qsol2=Qsol(:,2) ; % sol2: configuration du robot pour atteindre la piece
 
 % Affichage MGI
-figure(1)
-subplot(1,2,1)
+figure(4)
 drawBM(Qsol1)
 drawFrame(calculT04(Qsol1),'Routils',0.3)
-subplot(1,2,2)
+figure(5)
 drawBM(Qsol2)
 drawFrame(calculT04(Qsol2),'Routils',0.3)
 
 
 %% SECTION 2.3
-
-% Determination des representations d etat des actionneurs
-
 
 % Determination des representations d etat des actionneurs
 
@@ -131,22 +130,24 @@ C2=[1 0];
 
 D=0;
 
+% Mise sous forme EE
 EE1=ss(A1,B1,C1,D);
 EE2=ss(A2,B2,C2,D);
 
+% Valeur de consigne
 q1_star=Qsol1(1);
 q2_star=Qsol1(2);
 
         
 % Reponse indicielle des actionneurs seuls
-       
+figure(6)     
 subplot(1,2,1)
 plot(step(q1_star*EE1))
 subplot(1,2,2)
 plot(step(q2_star*EE2))
 
         
-%Calcul du retour d etat
+% Calcul du retour d etat
     
 Ar1=A1-B1*K1;
 Br1=B1*N1;
@@ -154,16 +155,16 @@ Br1=B1*N1;
 Ar2=A2-B2*K2;
 Br2=B2*N2;
 
+% Representation EE de la boucle fermee
 retour_et1=ss(Ar1,Br1,C1,D);
 retour_et2=ss(Ar2,Br2,C2,D);
 
+% Reponse indicielle pour la boucle fermee
+figure(7)
 subplot(1,2,1)
 plot(step(q1_star*retour_et1))
 subplot(1,2,2)
 plot(step(q2_star*retour_et2))
 	
-% Reponse indicielle de chaque actionneur et dessin de la trajectoire
-    
-
+% Dessin de la trajectoire 
 drawTraj(retour_et1,retour_et2,q1_star,q2_star,t,h,P_R0(1),P_R0(2),P_R0(3),1)
-    
