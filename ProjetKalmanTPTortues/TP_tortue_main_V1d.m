730bf20fc0e5f8a9995a4ae0cb1797614b01574f
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-10-25 09:58:34 +0200

    TP P1K : Loi de prédiction

--- ProjetKalmanTPTortues/TP_tortue_main_V1d.m
+++ ProjetKalmanTPTortues/TP_tortue_main_V1d.m
@@ -13,15 +13,24 @@ load turt8.data
 [N,T,Z,u,F,G,H,mX0,PX0,Qw,Rv,X]=simulationDonnees;
 
 
-
 %% Filtrage de Kalman 
 
 % Initialisation
 
-x_obsv={mX0}; 
-P_obsv={PX0};
+x_obsv=mX0; 
+P_obsv=PX0;
 
-% Prediction
+for k=1:(N-1)
+    F_predi=[1,0, (T(k+1)-T(k)), 0; 0, 1, 0, (T(k+1)-T(k)); 0,0,1,0; 0,0,0,1];
+    G_predi=[(T(k+1)-T(k)),0 ; 0,(T(k+1)-T(k)); 0,0; 0,0];
+
+    % Prediction
+    x_predi(:,k+1)=F_predi*x_obsv(:,k)+G_predi*u(:,k);
+    P_predi(k+4:k+7,:)=F_predi*P_obsv(k:k+3,:)*transpose(F_predi)+Qw %bruit de dynamqiue suppose staionnaire
+    
+    
+    
+end 
 
 
 
