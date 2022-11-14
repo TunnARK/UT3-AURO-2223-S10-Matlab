100497ef5a20122d696d8197ea875c3915246919
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-11-14 20:15:22 +0100

    COI Q2 Préparation du TP

--- CommandeOptimaleI/COI_TP1_ScriptMain_rawa.m
+++ CommandeOptimaleI/COI_TP1_ScriptMain_rawa.m
@@ -5,9 +5,28 @@
 
 
 %% Declaration des constantes
-
+Km=9;
+Tm=0.3;
+Kg=0.105;
+Ks=10;
+Ke=10;
 
 %% Modele EE
+A=[0 1 ; 0 -1/Tm];
+B=[0 ; (Km*Ke)/(9*Tm)];
+C=[Ks 0];
+D=[0]
+
+EE=ss(A,B,C,D)
 
 
 %% Etude de la stabilite 
+
+EE_vp=eig(A)
+
+%% Commandabilite et Observabilite
+
+Co=rank(ctrb(A,B))
+
+Ob=rank(obsv(A,C))
+
