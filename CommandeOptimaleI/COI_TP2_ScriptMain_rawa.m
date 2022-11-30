a7f11e0cc97180466d63f3fe17e691efaa7b378d
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-11-30 09:18:09 +0100

    COI TP2 : Initial Commit

---
+++ CommandeOptimaleI/COI_TP2_ScriptMain_rawa.m
@@ -0,0 +1,5 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TP2 Commande Optimale
+% RONK Antoine et GABRIEL CALIXTE Damien
+% Seance de 30 novembre 2022
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ CommandeOptimaleI/COI_TP2_ScriptMain_rawd.m
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TP2 Commande Optimale
+% RONK Antoine et GABRIEL CALIXTE Damien
+% Seance de 30 novembre 2022
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
