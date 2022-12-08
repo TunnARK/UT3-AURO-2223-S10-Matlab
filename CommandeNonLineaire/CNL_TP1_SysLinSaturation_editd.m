bb81f72552ea2b72c13b784046bceae52e8e7441
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-12-08 08:25:44 +0100

    TP1 CNL : Q2.4 needs fixing

--- CommandeNonLineaire/CNL_TP1_SysLinSaturation_editd.m
+++ CommandeNonLineaire/CNL_TP1_SysLinSaturation_editd.m
@@ -21,29 +21,36 @@ vp_A=eig(A)
 
 Tend=10; Tstep=0.001;
 time=0:Tstep:Tend;
-Xinit=[1;0]
 
-[tout,xout]=ode45(@fcNL,time,Xinit);
-plot(xout(:,1),xout(:,2))
 
+for Xinit = 0:5:100
+    
+    [tout,xout]=ode45(@fcNL,time,Xinit);
+    plot(xout(:,1),xout(:,2))    
+    hold on  
+end 
+
+hold off
+
+% Declaration de la fonction pour resolution
 
 function dxdt=fcNL(t,x)
-    A=[0 1 ; 1 0];
-    
+    A=[0 1 ; 1 0];    
+
     if (-2*x(1)+x(2))>1 
-        x(2)=x(1)+1
-        
+        x(2)=x(1)+1;
+        x(1)=x(2);
+        dxdt=x;
     else
         if (-2*x(1)+x(2))<-1
-           x(2)=x(1)-1
+           x(2)=x(1)-1;
+           x(1)=x(2);
+           dxdt=x;
         else 
-            x(2)=-x(1)-x(2)
+            x(2)=-x(1)-x(2);
+            x(1)=x(2);
+            dxdt=x;
         end
-    end
-    
-     dxdt=A*x;   
-     
-end         
-   
-    
+    end 
 
+end  
