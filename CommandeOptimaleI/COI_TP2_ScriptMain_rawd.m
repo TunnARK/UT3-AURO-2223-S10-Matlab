047ed6dcc51695e742a7e9d3856d951c341550d2
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-12-13 17:33:21 +0100

    TP2 COI : obsv & schéma bloc

--- CommandeOptimaleI/COI_TP2_ScriptMain_rawd.m
+++ CommandeOptimaleI/COI_TP2_ScriptMain_rawd.m
@@ -8,3 +8,35 @@
 clear all
 close all
 
+
+%% Constantes
+
+g=9.81;
+
+N1=3e-5;
+N2=0.5e-5;
+
+S1=0.0154;
+S2=0.0154;
+
+c1=0.4753;
+c2=0.9142;
+
+C1=c1*2*sqrt(2*g);
+C2=c2*2*sqrt(2*g);
+
+%% Modele lineaire 
+
+A=[(-1/S1)*C1*(1/2*sqrt(N1)) 0 ; (-1/S2)*C1*(1/2*sqrt(N1)) (-1/S2)*C2*(1/2*sqrt(N2))];
+
+B=[1/S1 ; 1/S2];
+
+C=[0 S2];
+
+D=[0];
+
+EE=ss(A,B,C,D)
+
+O=obsv(A,C);
+
+observabilite=rank(O)
